074c985f5308346d330fd1c2b5ddad22de3dfae8
Eslam Gamal <elkurdieslam@gmail.com>
2025-03-30 00:19:08 +0200

    `Disable DevicePreview and bump version to 1.0.0+7`

--- lib/main.dart
+++ lib/main.dart
@@ -39,7 +39,7 @@ void main() async {
 
   runApp(
     DevicePreview(
-      enabled: true, // تمكين DevicePreview
+      enabled: false, // تمكين DevicePreview
       builder: (context) => const MyApp(), // بناء التطبيق داخل DevicePreview
     ),
   );
